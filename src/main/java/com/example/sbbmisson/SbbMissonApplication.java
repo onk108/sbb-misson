d001f97f6d85417d99135f07cd60fa536382c6d7
onk108 <wkdtnstjr88@naver.com>
2023-11-21 11:13:22 +0900

    start

---
+++ src/main/java/com/example/sbbmisson/SbbMissonApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbbmisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMissonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMissonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbbmisson/SbbMissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbmisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMissonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
